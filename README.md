e4660134ff0cbd837ecdcbc92671ae7f1d376813
Lucas <boislucasnicolas@gmail.com>
2023-06-07 10:43:57 -0300

    root commit

--- README.md
+++
@@ -1,3 +0,0 @@
-<div align="center">
-  <img src="./frontend/src/assets/Logo.png" width="250">
-</div>
--- backend/index.js
+++
@@ -1,5 +0,0 @@
-import obs from './src/obs.js'
-import ws from './src/websocket.js'
-
-//obs.init()
-ws.init(9594)
